40a0ca8c8dd4b620b166c7d735e1cf49c8c56d5e
Kurilo Denis <kdb2015@mail.ru>
2019-02-11 17:14:07 +0300

    mobile

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 //jequry-ui
-$('.currency-list').selectmenu();
+//$('.currency-list').selectmenu();
 
 //slide2id - плавная прокрутка по ссылкам внутри страницы
 $("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
